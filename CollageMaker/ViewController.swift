4d96905c1cf2f33731e2137194a2fa71f9839edf
Vasili krasnoyski <vasili.krasnouski@gmail.com>
2025-01-29 12:08:01 +0200

    Initial Commit

---
+++ CollageMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollageMaker
+//
+//  Created by Vasili krasnoyski on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollageMakerTests/CollageMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollageMakerTests.swift
+//  CollageMakerTests
+//
+//  Created by Vasili krasnoyski on 29.01.2025.
+//
+
+import Testing
+@testable import CollageMaker
+
+struct CollageMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
